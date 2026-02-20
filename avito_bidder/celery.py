7e42df04140a16684a9d1ef1d168e891fca37b90
Affenshow <danik12062002@mail.ru>
2026-02-20 14:44:49 +0500

    celery.py

--- avito_bidder/celery.py
+++ avito_bidder/celery.py
@@ -12,3 +12,9 @@ app.config_from_object('django.conf:settings', namespace='CELERY')
 
 # Автоматически находить и подхватывать задачи из файлов tasks.py в наших приложениях.
 app.autodiscover_tasks()
+
+app.conf.task_routes = {
+    'main_app.tasks.run_bidding_for_task': {
+        'queue': lambda args, kwargs: f"task_{args[0]}"  # task_7919286919, task_7919282534 и т.д.
+    },
+}
